eb4c154ee8b8b3587ac55af6b5758a5074179f8d
John JD Donaldson <ntiersoftwareengineering@gmail.com>
2018-05-21 13:41:13 -0700

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour{
+
+    void OnCollisionEnter(Collision collision){
+        //var hit = collision.gameObject;
+        //var health = collision.gameObject.GetComponent<Health>();
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health != null) health.TakeDamage(10);
+
+        Destroy(gameObject);
+    }//OnCollisionEnter
+
+    
+    //void Start(){}
+    // Update is called once per frame
+    //void Update(){}
+
+}//class Bullet
